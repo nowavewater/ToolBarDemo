3cf01d427d9087df1f82a5a34389599dc55d44de
Pengcheng Yin <nowavewater@gmail.com>
2015-12-02 22:59:46 -0800

    Update Demo1Fragment.java

--- app/src/main/java/com/example/toolbardemo/Demo1Fragment.java
+++ app/src/main/java/com/example/toolbardemo/Demo1Fragment.java
@@ -37,13 +37,12 @@ public class Demo1Fragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        toolbar.setTitle(R.string.link_fragment1_text);
-        toolbar.setNavigationIcon(R.drawable.ic_back);
-        toolbar.inflateMenu(R.menu.menu_fragment1);
+        toolbar.setTitle(R.string.link_fragment1_text);   // Set toolbar title
+        toolbar.setNavigationIcon(R.drawable.ic_back);  // Set navigation icon
+        toolbar.inflateMenu(R.menu.menu_fragment1);    // Set toolbar menu
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {   // Set navigation listener
                 FragmentManager fm = getFragmentManager();
                 if (fm.getBackStackEntryCount() > 0) {
                     fm.popBackStack();
@@ -52,7 +51,7 @@ public class Demo1Fragment extends Fragment {
         });
         toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
+            public boolean onMenuItemClick(MenuItem menuItem) {  // Set toolbar menu listener
                 switch (menuItem.getItemId()) {
                     case R.id.fragment1_send:
                         Toast.makeText(getActivity(), "Send", Toast.LENGTH_LONG).show();
